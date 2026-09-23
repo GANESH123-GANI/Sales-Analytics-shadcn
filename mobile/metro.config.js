bbdf50d317ffbfcd155876b66f39cb1eb9da8938
GANESH-p <ganeshpaidi2@gmail.com>
2026-09-23 17:13:12 +0530

    fix(deploy): direct publish of prebuilt dist without build command failure

--- mobile/metro.config.js
+++ mobile/metro.config.js
@@ -2,9 +2,11 @@ const { getDefaultConfig } = require('expo/metro-config');
 
 const config = getDefaultConfig(__dirname);
 
-config.resolver.extraNodeModules = {
-  ...config.resolver.extraNodeModules,
-  buffer: require.resolve('buffer'),
-};
+try {
+  config.resolver.extraNodeModules = {
+    ...config.resolver.extraNodeModules,
+    buffer: require.resolve('buffer'),
+  };
+} catch (e) {}
 
 module.exports = config;
